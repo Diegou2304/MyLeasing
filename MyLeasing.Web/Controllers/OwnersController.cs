cfc71ef5b726c805d9e4652f9c7e6fba6ef49a6f
Diego Uribe <diegou2304-98@hotmail.com>
2020-02-26 13:14:30 -0400

    Details and create owners completed

--- MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing.Web/Controllers/OwnersController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data;
 using MyLeasing.Web.Data.Entities;
+using MyLeasing.Web.Helpers;
+using MyLeasing.Web.Models;
 
 namespace MyLeasing.Web.Controllers
 {
@@ -15,10 +17,13 @@ namespace MyLeasing.Web.Controllers
     public class OwnersController : Controller
     {
         private readonly DataContext _dataContext;
+        private readonly IUserHelper _userHelper;
 
-        public OwnersController(DataContext dataContext)
+        public OwnersController(DataContext dataContext,
+            IUserHelper userHelper)
         {
             _dataContext = dataContext;
+            _userHelper = userHelper;
         }
 
         // GET: Owners
@@ -64,20 +69,58 @@ namespace MyLeasing.Web.Controllers
             return View();
         }
 
-        // POST: Owners/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id")] Owner owner)
+        public async Task<IActionResult> Create(AddUserViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _dataContext.Add(owner);
-                await _dataContext.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+
+                var user = await CreateUserAsync(model);
+                if (user != null)
+                {
+                    var owner = new Owner
+                    {
+                        Contracts = new List<Contract>(),
+                        Properties = new List<Property>(),
+                        User = user,
+
+                    };
+
+                    _dataContext.Owners.Add(owner);
+                    await _dataContext.SaveChangesAsync();
+                    return RedirectToAction("Index");
+
+                }
+                ModelState.AddModelError(string.Empty, "User already exists");
             }
-            return View(owner);
+            return View(model);
+        }
+
+        private async Task<User> CreateUserAsync(AddUserViewModel model)
+        {
+            var user = new User()
+            {
+                Address = model.Address,
+                Document = model.Document,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Username,
+                Email = model.Username,
+
+
+
+            };
+            var result = await _userHelper.AddUserAsync(user, model.Password);
+            if(result.Succeeded)
+            {
+                user = await _userHelper.GetUserByEmailAsync(model.Username);
+                await _userHelper.AddUserToRoleAsync(user, "Owner");
+                return user;
+            }
+            return null;
         }
 
         // GET: Owners/Edit/5
---
+++ MyLeasing.Web/Models/AddUserViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Models
+{
+    public class AddUserViewModel
+    {
+        [Display(Name ="Email")]
+        [Required (ErrorMessage ="Este campo es requerido")]
+        [MaxLength(100,ErrorMessage ="no puede tener mas de 100 caracteres")]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Display(Name ="Document")]
+        [Required(ErrorMessage ="El campo es obligatorio")]
+        [MaxLength(50, ErrorMessage = "no puede tener mas de 50 caracteres")]
+        public string Document { get; set; }
+
+        [Display(Name ="First Name")]
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Last Name")]
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Display(Name ="Address")]
+        [MaxLength(100,ErrorMessage ="El campo no puede tener mas de 100 caracteres")]
+        public string Address { get; set; }
+
+        [Display(Name ="Phone Number")]
+        [MaxLength(50,ErrorMessage ="El campo no puede tener mas de 50 caracteres")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name ="Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20,MinimumLength =6, ErrorMessage ="Tiene que contener entre {2} y {1}")]
+        public string Password { get; set; }
+
+        [Display(Name = "Password Confirm")]
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Tiene que contener entre {2} y {1}")]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
